f0973af8da7159b4cb451bb57469e6912d7513e9
roflges <102219469+roflges@users.noreply.github.com>
2022-08-26 10:47:25 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,19 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        boolean first = bean == bean1;
+        boolean second = cat1 == cat2;
+        System.out.println(first);
+        System.out.println(second);
+
+
+
     }
 }
